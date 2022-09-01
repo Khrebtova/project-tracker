e291c211e8ecc1bd3998b8e0e255107671250317
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-09-01 16:20:11 -0700

    create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  resources :users
+  namespace :api do
+    resources :users
+    resources :todos
+
+    post '/signup', to: 'users#create'
+    get '/me', to: 'users#show'
+    
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
